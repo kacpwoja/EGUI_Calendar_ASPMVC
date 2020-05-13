02d9be4fabfbc2d8c05b7a3f1a03802db0720f64
kw <kacp.woja@gmail.com>
2020-05-13 17:58:11 +0200

    frontend start

--- EGUI_Calendar/Controllers/HomeController.cs
+++ EGUI_Calendar/Controllers/HomeController.cs
@@ -6,28 +6,63 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using EGUI_Calendar.Models;
+using EGUI_Calendar.Entities;
 
 namespace EGUI_Calendar.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly EventsContext events;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, EventsContext events)
         {
             _logger = logger;
+            this.events = events;
         }
 
-        public IActionResult Index()
+        public IActionResult Index(int? year, int? month)
         {
-            return View();
+            if(!year.HasValue || !month.HasValue)
+            {
+                return DefaultRedirect();
+            }
+
+            IndexViewModel vm;
+
+            try
+            {
+                var date = new DateTime(year.Value, month.Value, 1);
+                var isCurrent = DateTime.Now.Year == year.Value && DateTime.Now.Month == month.Value;
+
+                vm = new IndexViewModel
+                {
+                    Title = date.ToString("MMM - yyyy"),
+                    Offset = (int)date.DayOfWeek,
+                    Days = DateTime.DaysInMonth(year.Value, month.Value),
+                    SelectedDay = isCurrent ? DateTime.Now.Day : 0,
+                    BusyDays = events.GetBusyDays(year.Value, month.Value)
+                };
+            }
+            catch
+            {
+                return DefaultRedirect();
+            }
+
+            return View(vm);
         }
 
         public IActionResult Info()
         {
             return View();
         }
+
         public IActionResult Event()
+        {
+            return View();
+        }
+
+        public IActionResult Day(int? year, int? month, int? day)
         {
             return View();
         }
@@ -36,6 +71,14 @@ namespace EGUI_Calendar.Controllers
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+        private IActionResult DefaultRedirect()
+        {
+            var year = DateTime.Now.Year;
+            var month = DateTime.Now.Month;
+
+            return RedirectToAction(nameof(Index), new { year, month });
         }
     }
 }
---
+++ EGUI_Calendar/Models/IndexViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EGUI_Calendar.Controllers
+{
+    public class IndexViewModel
+    {
+        public string Title { get; set; }
+        public int Days { get; set; }
+        public int SelectedDay { get; set; }
+        public int[] BusyDays { get; set; }
+        public int Offset { get; set; }
+    }
+}
